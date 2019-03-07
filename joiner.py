c6a1c22e05ea9be004ce5ba61de0d81e57c3c6e0
BernardoGiordano <bernardogiordano369@gmail.com>
2019-03-07 23:09:12 +0100

    Push joiner script

---
+++ joiner.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import argparse
+import os
+import json
+
+parser = argparse.ArgumentParser(description = 'Sharkive cheat codes joiner')
+parser.add_argument('type', help = '3ds, switch')
+
+def main(args):
+    if '3ds' in args.type:
+        cheats = os.listdir('./3ds')
+    elif 'switch' in args.type:
+        cheats = os.listdir('./switch')
+    else:
+        exit(0)
+
+    db = {}
+    for cheat in cheats:
+        with open(os.path.join(args.type, cheat), 'r') as file:
+            titleid = cheat[:cheat.rfind('.')]
+            lines = [line.strip() for line in file]
+            lines = list(filter(None, lines))
+            
+            db[titleid] = {}
+            selectedCheat = lines[0]
+            for line in lines:
+                if line.startswith('[') and line.endswith(']'):
+                    selectedCheat = line[1:-1]
+                    db[titleid][selectedCheat] = []
+                else:
+                    db[titleid][selectedCheat].append(line)
+    with open(os.path.join('build', args.type + '.json'), 'w') as f:
+        f.write(json.dumps(db, indent = 2))
+
+if __name__ == '__main__':
+    main(parser.parse_args())
