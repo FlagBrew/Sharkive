1757bcbbd2f885e9ae776145ce27c8e3770f40e6
BernardoGiordano <bernardogiordano369@gmail.com>
2019-04-14 10:34:43 +0200

    Fix codes and complete joiner

--- joiner.py
+++ joiner.py
@@ -7,30 +7,51 @@ parser = argparse.ArgumentParser(description = 'Sharkive cheat codes joiner')
 parser.add_argument('type', help = '3ds, switch')
 
 def main(args):
+    db = {}
+
     if '3ds' in args.type:
-        cheats = os.listdir('./3ds')
+        for cheat in os.listdir('./3ds'):
+            with open(os.path.join(args.type, cheat), 'r') as file:
+                titleid = cheat[:cheat.rfind('.')]
+                lines = [line.strip() for line in file]
+                lines = list(filter(None, lines))
+                
+                db[titleid] = {}
+                selectedCheat = lines[0]
+                for line in lines:
+                    if line.startswith('{') and line.endswith('}'):
+                        pass
+                    elif line.startswith('[') and line.endswith(']'):
+                        selectedCheat = line[1:-1]
+                        db[titleid][selectedCheat] = []
+                    else:
+                        db[titleid][selectedCheat].append(line)
     elif 'switch' in args.type:
-        cheats = os.listdir('./switch')
+        for root, _, files in os.walk('./switch'):
+            titleid = root[root.rfind('/')+1:]
+
+            if "switch" in titleid:
+                continue
+
+            db[titleid] = {}
+            for file in files:
+                buildid = file[:file.find('.')]
+                db[titleid][buildid] = {}
+                with open(os.path.join(root, file), 'r') as f:
+                    lines = [line.strip() for line in f]
+                    lines = list(filter(None, lines))
+                    selectedCheat = lines[0]
+                    for line in lines:
+                        if line.startswith('{') and line.endswith('}'):
+                            pass
+                        elif line.startswith('[') and line.endswith(']'):
+                            selectedCheat = line[1:-1]
+                            db[titleid][buildid][selectedCheat] = []
+                        else:
+                            db[titleid][buildid][selectedCheat].append(line)
     else:
         exit(0)
 
-    db = {}
-    for cheat in cheats:
-        with open(os.path.join(args.type, cheat), 'r') as file:
-            titleid = cheat[:cheat.rfind('.')]
-            lines = [line.strip() for line in file]
-            lines = list(filter(None, lines))
-            
-            db[titleid] = {}
-            selectedCheat = lines[0]
-            for line in lines:
-                if line.startswith('{') and line.endswith('}'):
-                    pass
-                elif line.startswith('[') and line.endswith(']'):
-                    selectedCheat = line[1:-1]
-                    db[titleid][selectedCheat] = []
-                else:
-                    db[titleid][selectedCheat].append(line)
     with open(os.path.join('build', args.type + '.json'), 'w') as f:
         f.write(json.dumps(db))
 
